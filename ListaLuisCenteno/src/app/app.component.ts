1146384b29c9bed37310dbb10e9fccadc0162c4a
lcenteno33 <lcenteno@firstchairservices.com>
2022-07-27 22:33:25 -0300

    Adding deliverable nro 2

--- ListaLuisCenteno/src/app/app.component.ts
+++ ListaLuisCenteno/src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ListaLuisCenteno';
+  estudiantes = [
+    {
+      nombre: 'Estudiante 1',
+      apellido: 'Estudiante 11',
+      flagIcon: true,
+      flagClass: false
+    },{
+      nombre: 'Estudiante 2',
+      apellido: 'Estudiante 22',
+      flagIcon: false,
+      flagClass: true
+    },{
+      nombre: 'Estudiante 3',
+      apellido: 'Estudiante 33',
+      flagIcon: true,
+      flagClass: false
+    },
+    {
+      nombre: 'Estudiante 4',
+      apellido: 'Estudiante 44',
+      flagIcon: true,
+      flagClass: true
+    },
+    {
+      nombre: 'Estudiante 5',
+      apellido: 'Estudiante 55',
+      flagIcon: true,
+      flagClass: false
+    },
+    {
+      nombre: 'Estudiante 6',
+      apellido: 'Estudiante 66',
+      flagIcon: true,
+      flagClass: true
+    }
+  ];
 }
