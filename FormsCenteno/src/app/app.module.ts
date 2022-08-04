fc6381c5575298e3cd6a82af413489f6cde2db29
lcenteno33 <lcenteno@firstchairservices.com>
2022-08-03 23:27:01 -0300

    Form working, missing error messages

--- FormsCenteno/src/app/app.module.ts
+++ FormsCenteno/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MyformComponent } from './components/myform/myform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ FormsCenteno/src/app/components/myform/myform.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-myform',
+  templateUrl: './myform.component.html',
+  styleUrls: ['./myform.component.css']
+})
+export class MyformComponent implements OnInit {
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { 
+    this.registerForm = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      address: new FormControl('', [Validators.required]),
+      address2: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      state: new FormControl('', [Validators.required]),
+      zip: new FormControl('', [Validators.required]),
+      checkme: new FormControl(false, [Validators.required]),
+
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  sendForm () {
+    console.log(this.registerForm);
+  }
+
+}
