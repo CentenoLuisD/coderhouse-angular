2b1a3b809d8994c66434c797e49873614b96ea01
lcenteno33 <lcenteno@firstchairservices.com>
2022-08-21 22:25:59 -0300

    Creando entregable 2PFCenteno

---
+++ 2PFCenteno/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlumnosModule } from './alumnos/alumnos/alumnos.module';
+import { CursosModule } from './cursos/cursos/cursos.module';
+import { InscripcionesModule } from './inscripciones/inscripciones/inscripciones.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MainComponent } from './components/main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AlumnosModule,
+    CursosModule,
+    InscripcionesModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
