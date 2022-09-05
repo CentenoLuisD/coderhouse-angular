f7664cd7c1d273aa965f4eeddad2c4a6a08d4326
lcenteno33 <lcenteno@firstchairservices.com>
2022-09-05 15:17:01 -0300

    Test Unitario para el Servicio cursos service finalizada

--- 2PFCenteno/src/app/cursos/services/cursos.service.spec.ts
+++ 2PFCenteno/src/app/cursos/services/cursos.service.spec.ts
@@ -1,16 +1,75 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-// import { CursosService } from './cursos.service';
+import { CursosService } from './cursos.service';
+import { of } from 'rxjs';
 
-// describe('CursosService', () => {
-//   let service: CursosService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(CursosService);
-//   });
+describe('CursosService', () => {
+  let service: CursosService;
+  let httpClientSpy: { get: jasmine.Spy };
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        imports: [
+            HttpClientTestingModule
+          ]
+    });
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    // service = TestBed.inject(CursosService);
+    service = new CursosService(httpClientSpy as any)
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should be return all the Courses', () => {
+    const mockDatos = [
+        {
+            "name": "Angular 16",
+            "profesor": "Abner Garcia",
+            "id": "1"
+           },
+           {
+            "name": "Angular 16",
+            "profesor": "Lautaro Guerrero",
+            "id": "2"
+           },
+           {
+            "name": "MEMORY",
+            "profesor": "Millie",
+            "id": "3"
+           },
+           {
+            "name": "MEMORY",
+            "profesor": "Jacky",
+            "id": "4"
+           },
+           {
+            "name": "BLACKHOLE",
+            "profesor": "Mable",
+            "id": "5"
+           },
+           {
+            "name": "CSV",
+            "profesor": "Elta",
+            "id": "6"
+           },
+           {
+            "name": "BLACKHOLE",
+            "profesor": "Ursula",
+            "id": "7"
+           }
+    ];
+
+    httpClientSpy.get.and.returnValue(of(mockDatos));
+
+    service.obtenerCursos().subscribe((cursos) => {
+        expect(cursos).toEqual(mockDatos);
+    })
+
+  });
+
+
+});
--- 2PFCenteno/src/app/services/auth.service.spec.ts
+++ 2PFCenteno/src/app/services/auth.service.spec.ts
@@ -1,16 +1,52 @@
 // import { TestBed } from '@angular/core/testing';
+// import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 // import { AuthService } from './auth.service';
+// import { FormsModule } from '@angular/forms';
+// import { RouterTestingModule } from '@angular/router/testing';
+// import { from } from 'rxjs';
+// import { Usuario } from '../models/usuario';
+
 
 // describe('AuthService', () => {
 //   let service: AuthService;
+//   let router: RouterTestingModule;
+//   let httpClientSpy: {get: jasmine.Spy}
 
 //   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(AuthService);
+//     TestBed.configureTestingModule({
+//         imports: [
+//             HttpClientTestingModule, 
+//             FormsModule, 
+//             RouterTestingModule,
+//         ]
+//     });
+//     // let router: RouterTestingModule;
+//     httpClientSpy = jasmine.createSpyObj('HttpClient', ['get'])
+//     service = new AuthService(httpClientSpy as any, router as any);
 //   });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
+// //   it('should be created', () => {
+// //     expect(service).toBeTruthy();
+// //   });
+
+//   it('should return 1 user', (done: DoneFn) => {
+//    const mockUsuario: any = {
+//     "usuario": "Hartmann",
+//     "contrasena": "xVwUHB3NTKUrL01",
+//     "admin": false,
+//     "id": "1"
+//    };
+
+//    httpClientSpy.get.and.returnValue(from(mockUsuario));
+
+//    service.iniciarSesion(mockUsuario).subscribe((usuario: any) => {
+//     expect(usuario).toEqual(mockUsuario);
+//     done();
+//    })
+
 //   });
+
+
+
 // });
